20463a9f07540eab2be14d36dd2ff77b06d47a1b
MacOs <dangkimbao1999@gmail.com>
2023-11-25 15:17:58 +0700

    fix: erc1155 marketplace partial buy

--- src/mapping/Marketplace1155.ts
+++ src/mapping/Marketplace1155.ts
@@ -10,102 +10,80 @@ import {
 import { ERC1155Token, MarketEvent1155 } from "../../generated/schema"
 import { updateBlockEntity } from "../utils";
 
-function createEvent(id: BigInt, action: string): MarketEvent1155 {
-  return new MarketEvent1155(id.toString() + "-" + action);
-}
 
-function createEventId(id: BigInt, action: string): string {
-  return id.toString() + "-" + action;
-}
 export function handleAskNew(event: AskNew): void {
-  updateBlockEntity(event, 'handleAskNew-1155')
-  let ev = createEvent(event.params.askId, 'Ask');
+  let transaction = new MarketEvent1155(event.params.askId.toString())
   const nft = ERC1155Token.load(event.params.tokenId.toString());
-  log.info('nft1155: {}', [nft!.id])
   if (nft) {
-    log.info('creating new event', []);
-    ev.event = "AskNew";
-    ev.timestamp = event.block.timestamp;
-    ev.address = event.params.nft.toHexString();
-    ev.txHash = event.transaction.hash.toHex();
-    ev.from = event.params.seller.toHexString();
-    ev.nftId = nft.id;
-    ev.quoteToken = event.params.quoteToken. toHexString();
-    ev.price = event.params.pricePerUnit;
-    ev.amounts = event.params.quantity;
-    ev.operationId = event.params.askId;
-    ev.save();
+    transaction.operation = "Ask"
+    transaction.from = event.params.seller.toHexString();
+    transaction.nftId = nft.id.toString();
+    transaction.amounts = event.params.quantity
+    transaction.quoteToken = event.params.quoteToken.toHexString();
+    transaction.price = event.params.pricePerUnit
+    transaction.event = "AskNew"
+    transaction.txHash = event.transaction.hash.toHexString();
+    transaction.timestamp = event.block.timestamp
+    transaction.save()
+  }
+}
+export function handleOfferNew(event: OfferNew): void {
+  let transaction = new MarketEvent1155(event.params.offerId.toString())
+  const nft = ERC1155Token.load(event.params.tokenId.toString());
+  if (nft) {
+    transaction.operation = "Offer"
+    transaction.from = event.params.buyer.toHexString();
+    transaction.nftId = nft.id.toString();
+    transaction.amounts = event.params.quantity
+    transaction.quoteToken = event.params.quoteToken.toHexString();
+    transaction.price = event.params.pricePerUnit
+    transaction.event = "Bid"
+    transaction.txHash = event.transaction.hash.toHexString()
+    transaction.timestamp = event.block.timestamp
+    transaction.save()
   }
 }
 
 export function handleAskCancel(event: AskCancel): void {
-  updateBlockEntity(event, 'handleAskCancel-1155')
-  const id = createEventId(event.params.askId, 'Ask')
-  let ev = MarketEvent1155.load(id);
-  if (ev) {
-    ev.event = 'AskCancel';
-    ev.operationId = event.params.askId;
-    ev.save()
+  let transaction = MarketEvent1155.load(event.params.askId.toString())
+  if (transaction) {
+    transaction.event = "AskCancel"
+    transaction.save()
   }
 }
 
-export function handleTrade(event: Buy): void {
-  updateBlockEntity(event, 'handleTrade-1155')
-  let id = createEventId(event.params.askId, "Ask");
-  const ev = MarketEvent1155.load(id);
-  if (ev) {
-    ev.event = "Trade";
-    ev.timestamp = event.block.timestamp;
-    ev.txHash = event.transaction.hash.toHexString();
-    ev.to = event.params.buyer.toHexString();
-    ev.price = event.params.price;
-    ev.netPrice = event.params.netPrice;
-    ev.amounts = event.params.quantity;
-    ev.save();
+export function handleOfferCancel(event: OfferCancel): void {
+  let transaction = MarketEvent1155.load(event.params.offerId.toString())
+  if (transaction) {
+    transaction.event = "CancelBid"
+    transaction.save()
   }
 }
 
-export function handleAcceptBid(event: OfferAccept): void {
-  updateBlockEntity(event, 'handleAcceptBid-1155')
-  let id = createEventId(event.params.offerId, "Offer");
-  const ev = MarketEvent1155.load(id);
-  if (ev) {
-    ev.event = "AcceptBid";
-    ev.timestamp = event.block.timestamp;
-    ev.txHash = event.transaction.hash.toHexString();
-    ev.from = event.params.seller.toHexString();
-    ev.price = event.params.price;
-    ev.netPrice = event.params.netPrice;
-    ev.amounts = event.params.quantity;
-    ev.save();
+export function handleBuy(event: Buy): void {
+  let transaction = MarketEvent1155.load(event.params.askId.toString())
+  if (transaction) {
+    transaction.to = event.params.buyer.toHexString()
+    transaction.amounts = transaction.amounts.minus(event.params.quantity)
+    if (transaction.amounts.isZero()) {
+      transaction.event = "Trade"
+    } else {
+      transaction.event = "AskNew"
+    }
+    transaction.save()
   }
 }
 
-export function handleBid(event: OfferNew): void {
-  updateBlockEntity(event, 'handleCancelBid-1155')
-  let ev = createEvent(event.params.offerId, "Offer");
-  const nft = ERC1155Token.load(event.params.tokenId.toString());
-  if (nft) {
-    ev.event = "Bid";
-    ev.timestamp = event.block.timestamp;
-    ev.address = event.params.nft.toHexString();
-    ev.txHash = event.transaction.hash.toHexString();
-    ev.to = event.params.buyer.toHexString();
-    ev.nftId = nft.id;
-    ev.quoteToken = event.params.quoteToken.toHexString();
-    ev.price = event.params.pricePerUnit;
-    ev.amounts = event.params.quantity;
-    ev.save();
-  }
-}
-
-export function handleCancelBid(event: OfferCancel): void {
-  updateBlockEntity(event, 'handleCancelBid-1155')
-  const id = createEventId(event.params.offerId, 'Offer')
-  let ev = MarketEvent1155.load(id);
-  if (ev) {
-    ev.event = 'CancelBid';
-    ev.operationId = event.params.offerId;
-    ev.save()
+export function handleAcceptOffer(event: OfferAccept): void {
+  let transaction = MarketEvent1155.load(event.params.offerId.toString())
+  if (transaction) {
+    transaction.from = event.params.seller.toHexString();
+    transaction.amounts = transaction.amounts.minus(event.params.quantity)
+    if (transaction.amounts.isZero()) {
+      transaction.event = "Accepted"
+    } else {
+      transaction.event = "AcceptBid"
+    }
+    transaction.save()
   }
 }
