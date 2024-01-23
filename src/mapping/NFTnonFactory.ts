2f2cd5129c0bcebf410988273f74d72e4c107233
hieunm_u2u <hieunm@uniultra.xyz>
2024-01-23 10:00:20 +0700

    Fix TransferBath

--- src/mapping/NFTnonFactory.ts
+++ src/mapping/NFTnonFactory.ts
@@ -4,91 +4,92 @@ import { TransferSingle, TransferBatch } from "../../generated/templates/ERC1155
 import { Address, BigInt, log } from "@graphprotocol/graph-ts";
 import { fetchOrCreateAccount, generateCombineKey, updateBlockEntity, updateERC1155Balance, updateContractCount, updateOwnedTokenCount } from "../utils";
 import { ContractAddress } from "../enum";
-export function handleTransfer(event: Transfer): void {
-  if (event.params.to.toHexString() == ContractAddress.erc721marketplace) {
-    return;
-  }
-  let contract = ERC721Contract.load(event.address.toHex());
-  if (contract !== null) {
-    contract.name = null;
-    contract.symbol = null;
-    contract.save();
-  }
-  else {
-    let contract = new ERC721Contract(event.address.toHex());
-    contract.name = null;
-    contract.symbol = null;
-    contract.txCreation = "";
-    contract.count = BigInt.fromI32(0);
-    contract.asAccount = fetchOrCreateAccount(event.params.to).id;
-    contract.holderCount = BigInt.fromI32(0);
-    contract.createAt = event.block.timestamp;
-    contract.save()
-  }
-  if (event.params.from != Address.fromString(ContractAddress.ZERO) && event.params.to != Address.fromString(ContractAddress.erc721marketplace)) {
-    updateOwnedTokenCount(event.params.from.toHexString(), event.address.toHexString(), false, event.block.timestamp)
-  }
-  if (event.params.to != Address.fromString(ContractAddress.ZERO) && event.params.from != Address.fromString(ContractAddress.erc721marketplace)) {
-    updateOwnedTokenCount(event.params.to.toHexString(), event.address.toHexString(), true, event.block.timestamp)
-  }
-  log.warning('transfer nè: {} {} {}', [event.address.toHexString(), event.params.from.toHexString(), event.params.to.toHexString()])
-  let tokenId = generateCombineKey([event.address.toHexString(), event.params.tokenId.toString()]);
-  let token = ERC721Token.load(tokenId);
-  // // Handle the new transaction
-  let transaction = Transaction.load(event.transaction.hash.toHex());
-  if (transaction == null) {
-    transaction = new Transaction(event.transaction.hash.toHex());
-    transaction.timestamp = event.block.timestamp;
-    transaction.blockNumber = event.block.number;
-    transaction.save();
-  }
-
-  // If the token does not exist yet, create it - this indicates a minting event
-  if (token == null) {
-    token = new ERC721Token(tokenId);
-    token.tokenId = event.params.tokenId.toString()
-    token.contract = event.address.toHexString();
-    token.identifier = event.params.tokenId;
-    token.owner = event.params.to.toHexString();
-    token.txCreation = event.transaction.hash.toHexString()
-    let zeroAccount = fetchOrCreateAccount(Address.fromString(ContractAddress.ZERO));
-    token.createAt = event.block.timestamp;
-    updateBlockEntity(event, event.address, event.params.tokenId, event.params.from, event.params.to, 'Mint', BigInt.fromI32(0), BigInt.fromI32(1), Address.fromString(ContractAddress.ZERO));
-    updateContractCount(event.address.toHexString(), BigInt.fromI32(1), 'ERC721');
-    // Set the approval to the zero address when the token is minted
-    token.approval = zeroAccount.id;
-    token.uri = ""; // Set the URI based on your logic
-    token.save();
-  } else {
-    // Assuming approval is cleared on transfer
-    token.approval = '0x0000000000000000000000000000000000000000';
-    token.owner = event.params.to.toHex(); // Update the owner
-    if (event.params.from.toHexString() != ContractAddress.erc721marketplace) {
-      updateBlockEntity(event, event.address, event.params.tokenId, event.params.from, event.params.to, 'Transfer', BigInt.fromI32(0), BigInt.fromI32(1), Address.fromString(ContractAddress.ZERO));
+  export function handleTransfer(event: Transfer): void {
+    if (event.params.to.toHexString() == ContractAddress.erc721marketplace) {
+      return;
     }
-    token.save();
+    let contract = ERC721Contract.load(event.address.toHex());
+    if (contract !== null) {
+      contract.name = null;
+      contract.symbol = null;
+      contract.save();
+    }
+    else {
+      let contract = new ERC721Contract(event.address.toHex());
+      contract.name = null;
+      contract.symbol = null;
+      contract.txCreation = event.transaction.hash.toHexString();
+      contract.count = BigInt.fromI32(0);
+      contract.asAccount = fetchOrCreateAccount(event.params.to).id;
+      contract.holderCount = BigInt.fromI32(0);
+      contract.createAt = event.block.timestamp;
+      contract.save()
+    }
+    if (event.params.from != Address.fromString(ContractAddress.ZERO) && event.params.to != Address.fromString(ContractAddress.erc721marketplace)) {
+      updateOwnedTokenCount(event.params.from.toHexString(), event.address.toHexString(), false, event.block.timestamp)
+    }
+    if (event.params.to != Address.fromString(ContractAddress.ZERO) && event.params.from != Address.fromString(ContractAddress.erc721marketplace)) {
+      updateOwnedTokenCount(event.params.to.toHexString(), event.address.toHexString(), true, event.block.timestamp)
+    }
+    log.warning('transfer nè: {} {} {}', [event.address.toHexString(), event.params.from.toHexString(), event.params.to.toHexString()])
+    let tokenId = generateCombineKey([event.address.toHexString(), event.params.tokenId.toString()]);
+    let token = ERC721Token.load(tokenId);
+    // // Handle the new transaction
+    let transaction = Transaction.load(event.transaction.hash.toHex());
+    if (transaction == null) {
+      transaction = new Transaction(event.transaction.hash.toHex());
+      transaction.timestamp = event.block.timestamp;
+      transaction.blockNumber = event.block.number;
+      transaction.save();
+    }
+
+    // If the token does not exist yet, create it - this indicates a minting event
+    if (token == null) {
+      token = new ERC721Token(tokenId);
+      token.tokenId = event.params.tokenId.toString()
+      token.contract = event.address.toHexString();
+      token.identifier = event.params.tokenId;
+      token.owner = event.params.to.toHexString();
+      token.txCreation = event.transaction.hash.toHexString()
+      token.createAt = event.block.timestamp;
+      let zeroAccount = fetchOrCreateAccount(Address.fromString(ContractAddress.ZERO));
+      updateBlockEntity(event, event.address, event.params.tokenId, event.params.from, event.params.to, 'Mint', BigInt.fromI32(0), BigInt.fromI32(1), Address.fromString(ContractAddress.ZERO));
+      updateContractCount(event.address.toHexString(), BigInt.fromI32(1), 'ERC721');
+      // Set the approval to the zero address when the token is minted
+      token.approval = zeroAccount.id;
+      token.uri = ""; // Set the URI based on your logic
+      token.save();
+    } else {
+      // Assuming approval is cleared on transfer
+      token.approval = '0x0000000000000000000000000000000000000000';
+      token.owner = event.params.to.toHex(); // Update the owner
+      if (event.params.from.toHexString() != ContractAddress.erc721marketplace) {
+        updateBlockEntity(event, event.address, event.params.tokenId, event.params.from, event.params.to, 'Transfer', BigInt.fromI32(0), BigInt.fromI32(1), Address.fromString(ContractAddress.ZERO));
+      }
+      token.save();
+    }
+
+    // Ensure 'to' account is created
+    // Ensure 'to' account is created
+    let accountTo = fetchOrCreateAccount(event.params.to)
+
+    // Create the transfer event entity
+    let transferId = generateCombineKey([event.transaction.hash.toHex(), event.address.toHex(), tokenId])
+    let transfer = new ERC721Transfer(transferId);
+    transfer.transaction = transaction.id;
+    transfer.contract = event.address.toHex();
+    transfer.token = tokenId;
+    transfer.to = accountTo.id;
+    transfer.from = event.params.from.toHex();
+    transfer.timestamp = event.block.timestamp;
+    transfer.emitter = event.params.from.toHex();
+    transfer.save();
+
+    // Ensure 'from' account is created
+    if (event.params.from.toHex() != "0x0000000000000000000000000000000000000000") {
+      let accountFrom = fetchOrCreateAccount(event.params.from);
+    }
   }
-
-  // Ensure 'to' account is created
-  let accountTo = fetchOrCreateAccount(event.params.to)
-
-  // Create the transfer event entity
-  let transferId = generateCombineKey([event.transaction.hash.toHex(), event.address.toHex(), tokenId])
-  let transfer = new ERC721Transfer(transferId);
-  transfer.transaction = transaction.id;
-  transfer.contract = event.address.toHex();
-  transfer.token = tokenId;
-  transfer.to = accountTo.id;
-  transfer.from = event.params.from.toHex();
-  transfer.timestamp = event.block.timestamp;
-  transfer.emitter = event.params.from.toHex();
-  transfer.save();
-
-  // Ensure 'from' account is created
-  if (event.params.from.toHex() != "0x0000000000000000000000000000000000000000") {
-    let accountFrom = fetchOrCreateAccount(event.params.from);
-  }
-}
   export function handleTransferSingleNoFactory(event: TransferSingle): void {
     if (event.params.to.toHexString() == ContractAddress.erc1155marketplace) {
       return;
@@ -103,7 +104,7 @@ export function handleTransfer(event: Transfer): void {
       let contract = new ERC1155Contract(event.address.toHex());
       contract.name = null;
       contract.symbol = null;
-      contract.txCreation = "";
+      contract.txCreation = event.transaction.hash.toHexString();
       contract.createAt = event.block.timestamp;
       contract.count = BigInt.fromI32(0);
       contract.asAccount = fetchOrCreateAccount(event.params.to).id;
@@ -197,7 +198,7 @@ export function handleTransfer(event: Transfer): void {
       let contract = new ERC1155Contract(event.address.toHex());
       contract.name = null;
       contract.symbol = null;
-      contract.txCreation = "";
+      contract.txCreation = event.transaction.hash.toHexString();
       contract.count = BigInt.fromI32(0);
       contract.createAt = event.block.timestamp;
       contract.asAccount = fetchOrCreateAccount(event.params.to).id;
@@ -230,7 +231,16 @@ export function handleTransfer(event: Transfer): void {
         totalSupply.token = token.id;
         totalSupply.save();
 
-      token.totalSupply = totalSupply.id;
+        token.totalSupply = totalSupply.id;
+        if (event.params.from == Address.fromString(ContractAddress.ZERO)) {
+          let balanceId = generateCombineKey([event.address.toHex(), event.params.ids[i].toString()])
+          let totalSupply = ERC1155Balance.load(balanceId);
+          if (totalSupply) {
+            totalSupply.value = totalSupply.value.plus(event.params.values[i].toBigDecimal());
+            totalSupply.valueExact = totalSupply.valueExact.plus(event.params.values[i]);
+            totalSupply.save();
+          }
+        }
         token.save();
       }
   
